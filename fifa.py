71c853aa9aaa2885f2f191efd12da19b6dbb843a
Peter Czimmermann <xczimi@gmail.com>
2010-04-30 09:46:14 -0700

    munge KO games

--- fifa.py
+++ fifa.py
@@ -2,11 +2,11 @@ import os
 import re
 import xpath
 from xml.dom.minidom import parse, parseString
-from datetime import datetime 
+from datetime import datetime
 
 def read_clean_file(filename):
     """Read the filename and make sure it's parsable as xml.
-    
+
     This currently means removing lines containing document.write"""
     clean = ''
     f = open(filename)
@@ -37,29 +37,30 @@ def get_team(match, home_or_away):
     elif re.match(r'^[12][A-H]$', td_value):
         new_team['reference'] = {'rank': int(td_value[0]) , 'game_ref' : "Group "+td_value[1]}
     elif re.match(r'W([0-9]+)$', td_value):
-        new_team['reference'] = {'rank': 1 , 'game_id' : int(td_value[1:])}
+        new_team['reference'] = {'rank': 1 , 'game_ref' : "KO "+td_value[1:]}
+    elif re.match(r'L([0-9]+)$', td_value):
+        new_team['reference'] = {'rank': 2 , 'game_ref' : "KO "+td_value[1:]}
     else:
         new_team['reference'] = td_value
     return new_team
-    
+
 def get_games(stage):
     dom = get_dom(stage)
     games = []
     for groupdom in dom:
         group_name = xpath.findvalue('caption',groupdom)
         for match in xpath.find('tbody/tr',groupdom):
-            new_game = {'id' : xpath.findvalue('td[contains(@class,"mNum")]',match), 
+            new_game = {'id' : xpath.findvalue('td[contains(@class,"mNum")]',match),
                         'group' : group_name ,
                         'time' : timestring_to_datetime(xpath.findvalue('td[contains(@class,"dt")]/span/@title',match).split(",")[1].encode('utf-8')) ,
                         'location' : xpath.findvalue('td/a[contains(@href,"destination")]',match).strip() ,
                         }
             for home_or_away in ['home','away']:
                 new_game[home_or_away + '_team'] = get_team(match, home_or_away)
-            print new_game
-            games = games + [new_game]
+            games.append(new_game)
     return games
 
 def get_all_games():
     return get_games("index") + get_games("kostage")
-    
+
 #get_all_games()
