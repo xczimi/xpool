6738554a29315ed3c2bb8a83612b19242d69b1fb
Peter Czimmermann <xczimi@gmail.com>
2026-05-19 09:51:44 -0700

    feat(infra): private S3 bucket for SPA assets

---
+++ infrastructure/s3.tf
@@ -0,0 +1,17 @@
+module "spa_bucket" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "~> 4.1"
+
+  bucket = "xpool-spa-${var.environment}-${data.aws_caller_identity.current.account_id}"
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+
+  versioning = {
+    enabled = false
+  }
+}
+
+data "aws_caller_identity" "current" {}
